a703e543dd2593a02e972d7904528a7fdbe52cf1
rahul.cyber <rahul.cyber2023@outlook.com>
2025-06-09 19:00:08 +0530

    otp

--- components/Patient_Header/Patient_Header.js
+++ components/Patient_Header/Patient_Header.js
@@ -1,17 +1,28 @@
 import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const Patient_Header = () => {
+const Patient_Header = ({ to = null, title = null }) => {
+  const router = useRouter();
+  const redirectHandler = (to) => {
+    if (to) {
+      router.push(to);
+    }
+  };
   return (
     <>
-    <div  className="">
-      <div className="flex items-center py-4 px-0 gap-[9px]">
-        <ChevronLeft size={24} className=" text-black-700" />
-        <div className="flex-1 text-[14px] font-semibold text-gray-800">
-          Add New Patient
+      <div>
+        <div className="flex items-center py-4 px-0 gap-[9px]">
+          <ChevronLeft
+            size={24}
+            className=" text-black-700"
+            onClick={() => redirectHandler(to)}
+          />
+          <div className="flex-1 text-[14px] font-semibold text-gray-800">
+            {title}
+          </div>
+          <div className="h-6 w-6" /> {/* Space */}
         </div>
-        <div className="h-6 w-6" /> {/* Space */}
-      </div>
       </div>
     </>
   );
